eabe39badd7e8374c37e7fa4f43b8687f195c63e
heshamtamer <hesham1762002@yahoo.com>
2024-11-02 14:02:55 +0200

    schedule added

--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@ const express = require('express');
 const cors = require('cors');
 const connectDb = require("./config/dbConnection");
 const dotenv = require("dotenv").config();
-
+const cron = require("node-cron");
+const Order = require("./models/Order"); // Import Order model
 // Connect to the database
 connectDb();
 
@@ -20,6 +21,16 @@ app.get('/', (req, res) => {
 });
 app.use("/api/orders", require("./routes/orderRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
+
+// Schedule cron job to delete orders every 12 hours
+cron.schedule("0 */12 * * *", async () => {
+    try {
+        await Order.deleteMany({});
+        console.log("Orders collection cleared successfully.");
+    } catch (error) {
+        console.error("Error deleting orders:", error);
+    }
+});
 
 // Start server
 app.listen(PORT,'0.0.0.0', () => console.log(`Server running on port ${PORT}`));
