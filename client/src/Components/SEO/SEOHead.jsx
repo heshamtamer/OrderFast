6cdc487d283aeba3d68cb3ea2f614cd874bc59df
heshamtamer <hesham1762002@yahoo.com>
2025-09-06 16:15:49 +0300

    edites in SEO

--- client/src/Components/SEO/SEOHead.jsx
+++ client/src/Components/SEO/SEOHead.jsx
@@ -13,48 +13,72 @@ const SEOHead = ({
     document.title = title;
     
     // Update meta description
-    const metaDescription = document.querySelector('meta[name="description"]');
-    if (metaDescription) {
-      metaDescription.setAttribute('content', description);
+    let metaDescription = document.querySelector('meta[name="description"]');
+    if (!metaDescription) {
+      metaDescription = document.createElement('meta');
+      metaDescription.setAttribute('name', 'description');
+      document.head.appendChild(metaDescription);
     }
+    metaDescription.setAttribute('content', description);
     
     // Update meta keywords
-    const metaKeywords = document.querySelector('meta[name="keywords"]');
-    if (metaKeywords) {
-      metaKeywords.setAttribute('content', keywords);
+    let metaKeywords = document.querySelector('meta[name="keywords"]');
+    if (!metaKeywords) {
+      metaKeywords = document.createElement('meta');
+      metaKeywords.setAttribute('name', 'keywords');
+      document.head.appendChild(metaKeywords);
     }
+    metaKeywords.setAttribute('content', keywords);
     
     // Update Open Graph tags
-    const ogTitle = document.querySelector('meta[property="og:title"]');
-    if (ogTitle) {
-      ogTitle.setAttribute('content', title);
+    let ogTitle = document.querySelector('meta[property="og:title"]');
+    if (!ogTitle) {
+      ogTitle = document.createElement('meta');
+      ogTitle.setAttribute('property', 'og:title');
+      document.head.appendChild(ogTitle);
     }
+    ogTitle.setAttribute('content', title);
     
-    const ogDescription = document.querySelector('meta[property="og:description"]');
-    if (ogDescription) {
-      ogDescription.setAttribute('content', description);
+    let ogDescription = document.querySelector('meta[property="og:description"]');
+    if (!ogDescription) {
+      ogDescription = document.createElement('meta');
+      ogDescription.setAttribute('property', 'og:description');
+      document.head.appendChild(ogDescription);
     }
+    ogDescription.setAttribute('content', description);
     
-    const ogUrl = document.querySelector('meta[property="og:url"]');
-    if (ogUrl) {
-      ogUrl.setAttribute('content', url);
+    let ogUrl = document.querySelector('meta[property="og:url"]');
+    if (!ogUrl) {
+      ogUrl = document.createElement('meta');
+      ogUrl.setAttribute('property', 'og:url');
+      document.head.appendChild(ogUrl);
     }
+    ogUrl.setAttribute('content', url);
     
     // Update Twitter tags
-    const twitterTitle = document.querySelector('meta[property="twitter:title"]');
-    if (twitterTitle) {
-      twitterTitle.setAttribute('content', title);
+    let twitterTitle = document.querySelector('meta[property="twitter:title"]');
+    if (!twitterTitle) {
+      twitterTitle = document.createElement('meta');
+      twitterTitle.setAttribute('property', 'twitter:title');
+      document.head.appendChild(twitterTitle);
     }
+    twitterTitle.setAttribute('content', title);
     
-    const twitterDescription = document.querySelector('meta[property="twitter:description"]');
-    if (twitterDescription) {
-      twitterDescription.setAttribute('content', description);
+    let twitterDescription = document.querySelector('meta[property="twitter:description"]');
+    if (!twitterDescription) {
+      twitterDescription = document.createElement('meta');
+      twitterDescription.setAttribute('property', 'twitter:description');
+      document.head.appendChild(twitterDescription);
     }
+    twitterDescription.setAttribute('content', description);
     
-    const twitterUrl = document.querySelector('meta[property="twitter:url"]');
-    if (twitterUrl) {
-      twitterUrl.setAttribute('content', url);
+    let twitterUrl = document.querySelector('meta[property="twitter:url"]');
+    if (!twitterUrl) {
+      twitterUrl = document.createElement('meta');
+      twitterUrl.setAttribute('property', 'twitter:url');
+      document.head.appendChild(twitterUrl);
     }
+    twitterUrl.setAttribute('content', url);
     
     // Update canonical URL
     let canonical = document.querySelector('link[rel="canonical"]');
